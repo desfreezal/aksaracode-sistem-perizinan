ec8e8a0f3f4ee59f161574335c1622e58ea79ef9
Nur Cholis <114427619+OlisUnesa062@users.noreply.github.com>
2023-11-25 21:43:33 +0700

    terbaru

--- resources/views/operator/dashboard.blade.php
+++ resources/views/operator/dashboard.blade.php
@@ -17,10 +17,9 @@
         <li>
           <div class="parent">
             <a href="#">
-            Home 
             <img
             class="images"
-              src="{{asset('home/img/Home Logo.png')}}"
+              src="{{('home/img/Notif Logo.png')}}"
               alt="Notif Logo"
               style="width: 25px"
             ></a>
@@ -29,79 +28,27 @@
         </li>
         <li>
           <a href="#"
-            >Profile<img
+            ><img
               src="{{asset('home/img/Profil User Logo.png')}}"
               alt="Logo Profil"
               style="width: 25px"
             /></a
           >
         </li>
+        <li>
+          <a href="#"
+            >Micella</a
+          >
+        </li>
       </ul>
     </nav>
     <hr />
     <!-- Navbar Dashboard -->
-    <div class="container-kotak-cokelat" style="gap: 10px; margin: 80px 50px">
-      <div class="kotak-cokelat">
-        <div class="daftar-ulang">
-          <div class="gambar-bulat">
-            <img src="{{asset('home/img/Folder.png')}}" alt="LogoDaftarUlang" />
-          </div>
-          <div class="tombol-putih">
-            <a href="#">Ajukan Permohonan</a>
-          </div>
-        </div>
-      </div>
-      <div class="kotak-cokelat-1">
-        <div class="izin-pendirian">
-          <div class="gambar-bulat">
-            <img src="{{asset('home/img/School.png')}}" alt="LogoIzinPendirian" />
-          </div>
-          <div class="tombol-putih">
-            <a href="#">Status Permohonan</a>
-          </div>
-        </div>
-      </div>
-      <div class="kotak-cokelat-2">
-        <div class="izin-operasional">
-          <div class="gambar-bulat">
-            <img src="{{asset('home/img/Compliant.png')}}" alt="LogoIzinOperasional" />
-          </div>
-          <div class="tombol-putih">
-            <a href="#">Kelengkapan Data</a>
-          </div>
-        </div>
-      </div>
-      <div class="kotak-cokelat-3">
-        <div class="izin-pendirian">
-          <div class="gambar-bulat">
-            <img src="{{asset('home/img/School.png')}}" alt="LogoIzinPendirian" />
-          </div>
-          <div class="tombol-putih">
-            <a href="#">Validasi Data</a>
-          </div>
-        </div>
-      </div>
-      <div class="kotak-cokelat-4">
-        <div class="izin-pendirian">
-          <div class="gambar-bulat">
-            <img src="{{asset('home/img/School.png')}}" alt="LogoIzinPendirian" />
-          </div>
-          <div class="tombol-putih">
-            <a href="#">Survey</a>
-          </div>
-        </div>
-      </div>
-      <div class="kotak-cokelat-5">
-        <div class="izin-pendirian">
-          <div class="gambar-bulat">
-            <img src="{{asset('home/img/School.png')}}" alt="LogoIzinPendirian" />
-          </div>
-          <div class="tombol-putih">
-            <a href="#">Dashboard Statistik</a>
-          </div>
-        </div>
-      </div>
+    <div class="rectangle-493">
+      <img src="{{('home/operator/img/icon pengajuan permohonan.png')}}" alt="pengajuan permohonan" />
+      <p>Pengajuan Permohonan</p>
     </div>
+
     <!-- Footer -->
     <div class="footer" style="padding-top: 0px">
       <div class="edu-license-2023">EduLicense 2023</div>

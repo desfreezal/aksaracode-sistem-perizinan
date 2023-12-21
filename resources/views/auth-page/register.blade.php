303e5c4d8a10bfe9037ccca11a0fb43e9ad212ea
AlgonzaDJun <algonsadewangga@gmail.com>
2023-12-21 17:07:16 +0900

    finishing

--- resources/views/auth-page/register.blade.php
+++ resources/views/auth-page/register.blade.php
@@ -14,7 +14,7 @@
 
                         <div class="rounded-2xl pt-5 mx-7 mb-28 flex flex-col items-center border border-black" id="login-form">
                             <h1 class="font-bold text-primary text-4xl">BUAT AKUN</h1>
-                            <form class="flex flex-col gap-y-5 my-5 px-10 w-full">
+                            <form class="flex flex-col gap-y-5 my-5 px-10 w-full" action="/login" method="GET">
                                 <input type="text" placeholder="Nama Lengkap (Sesuai Identitas)"
                                     class="border border-black px-8 py-1 rounded-xl">
                                 <input type="text" placeholder="Email" class="border border-black px-8 py-1 rounded-xl">

3504d0ed4b2def815bb62d11ad6cf2c9a27b38d9
yandiaan <diansetiawan2121@gmail.com>
2023-12-29 19:52:53 +0700

    feature crud operasional core

--- app/Models/Operasional.php
+++ app/Models/Operasional.php
@@ -8,4 +8,59 @@ use Illuminate\Database\Eloquent\Model;
 class Operasional extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+     {
+         parent::boot();
+     
+         static::creating(function ($daftarUlang) {
+             if (!$daftarUlang->id) {
+                 $timestamp = now()->format('YmdHis');
+                 $daftarUlang->id = 'C-' . $timestamp;
+             }
+         });
+     }
+
+    protected $table = 'operasional';
+
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'statusDokumen_id',
+        'longtitude',
+        'latitude',
+        'lokasi',
+        'dokumen_survey',
+        'Super_Izin_Operasional',
+        'Struk_Organisasi',
+        'Akta_Notaris',
+        'Surat_IMB',
+        'Akta_Pendirian',
+        'Super_Bermaterai',
+        'Data_Anak_Dididk',
+        'Data_Pendidik',
+        'Rencana_Jadwal',
+        'Doc_Milik_Lahan',
+        'Doc_Bukti_Hukum',
+        'BPJS',
+        'Fotokopi_Rekening',
+        'Data_Daya_Tampung',
+        'SK_Izin_Pendirian',
+        'Piagam_Akreditasi',
+        'created_at',
+        'updated_at',
+    ];
+
+    public $incrementing = false; 
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    protected $hidden = ['user'];
+
+    protected $casts = [
+        'id' => 'string',
+    ];
 }

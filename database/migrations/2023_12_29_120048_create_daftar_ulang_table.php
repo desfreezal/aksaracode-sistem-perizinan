d9301b499d865382defafa5b5ecff93778c03c64
yandiaan <diansetiawan2121@gmail.com>
2023-12-29 19:27:08 +0700

    feat crud daftar ulang

---
+++ database/migrations/2023_12_29_120048_create_daftar_ulang_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('daftar_ulang', function (Blueprint $table) {
+            $table->string('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('statusDokumen_id');
+            $table->string('longtitude')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('lokasi')->nullable();
+            $table->string('dokumen_survey')->nullable();
+            $table->string('Super_Daftar_Ulang')->nullable();
+            $table->string('SK_Izin_Pendirian')->nullable();
+            $table->string('SK_Izin_Operasional')->nullable();
+            $table->string('Akta_Notaris')->nullable();
+            $table->string('Surat_IMB')->nullable();
+            $table->string('Data_Anak_Dididk')->nullable();
+            $table->string('Data_Pendidik')->nullable();
+            $table->string('Rencana_Jadwal')->nullable();
+            $table->string('Fotokopi_Rekening')->nullable();
+            $table->string('Super_Bermaterai')->nullable();
+            $table->string('Piagam_Akreditasi')->nullable();
+            $table->string('BPJS')->nullable();
+            $table->string('Data_Daya_Tampung')->nullable();
+            $table->string('Doc_Milik_Lahan')->nullable();
+            $table->string('Doc_Bukti_Hukum')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('category_pendirian')->onDelete('cascade');
+            $table->foreign('statusDokumen_id')->references('id')->on('status_dokumen')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daftar_ulang');
+    }
+};

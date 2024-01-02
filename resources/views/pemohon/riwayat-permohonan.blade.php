7a4708222fc3eb4781f3edc1cbd61c16d1a4c7ca
AlgonzaDJun <algonsadewangga@gmail.com>
2024-01-02 13:37:19 +0900

    sidebar profil

--- resources/views/pemohon/riwayat-permohonan.blade.php
+++ resources/views/pemohon/riwayat-permohonan.blade.php
@@ -6,7 +6,7 @@
     <div class="min-h-screen h-full">
         <div class="flex">
             {{-- SIDEBAR --}}
-            {{-- @include('pemohon.sidebar') --}}
+            @include('pemohon.sidebar')
             {{-- SIDEBAR --}}
 
             <div class="flex-grow h-full pl-[34px] pr-[88px] py-7 box-border mb-10" id="content">
